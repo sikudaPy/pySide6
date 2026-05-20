585b739d3308429de25d2b559d072914405b81de
sikudamobile <ya.sikuda@yandex.ru>
2026-05-20 16:43:45 +0300

    TableModel for request

--- __simple/HTTP/request.py
+++ __simple/HTTP/request.py
@@ -4,6 +4,6 @@ def shorten_url(url):
     response = requests.get(url)
     return response.text
 
-url = "http://php1c.ru/catalogs/api?format=json"
+url = "https://python1c.ru/catalogs/api?format=json"
 short_url = shorten_url(url)
 print(f"The shortened URL is: {short_url}")
